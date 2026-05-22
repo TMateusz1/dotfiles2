4703130d337a2a060ef9094d3a7bd9e7ee278345
Mateusz Tarasewicz <git@mail.tmateusz.pl>
2026-05-22 14:39:17 +0200

    md-reader

---
+++ nvim/lua/plugins/md-reader.lua
@@ -0,0 +1,61 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		ft = { "markdown" },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-mini/mini.icons",
+			-- alternatywnie:
+			-- "nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			file_types = { "markdown" },
+			render_modes = { "n", "c", "t" },
+			completions = {
+				lsp = {
+					enabled = true,
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>md",
+				"<cmd>RenderMarkdown toggle<cr>",
+				desc = "Markdown: toggle rendered view",
+			},
+			{
+				"<leader>mD",
+				"<cmd>RenderMarkdown preview<cr>",
+				desc = "Markdown: preview in side window",
+			},
+		},
+	},
+
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = {
+			"MarkdownPreviewToggle",
+			"MarkdownPreview",
+			"MarkdownPreviewStop",
+		},
+		ft = { "markdown" },
+		build = function()
+			require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+			vim.fn["mkdp#util#install"]()
+		end,
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+			vim.g.mkdp_auto_start = 0
+			vim.g.mkdp_auto_close = 0
+			vim.g.mkdp_refresh_slow = 0
+		end,
+		keys = {
+			{
+				"<leader>me",
+				"<cmd>MarkdownPreviewToggle<cr>",
+				ft = "markdown",
+				desc = "Markdown: browser preview",
+			},
+		},
+	},
+}
