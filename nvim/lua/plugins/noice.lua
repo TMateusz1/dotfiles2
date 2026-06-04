0ce6de5a182ce5e0d78c650cac21304b36abb404
Mateusz Tarasewicz <git@mail.tmateusz.pl>
2026-06-04 22:45:12 +0200

    noice

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -2,47 +2,28 @@ return {
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
+		opts = {
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+			-- add any options here
+		},
 		dependencies = {
 			"MunifTanjim/nui.nvim",
-		},
-		opts = {
-			cmdline = {
-				enabled = true,
-				view = "cmdline",
-			},
-			messages = {
-				enabled = true,
-				view = "mini",
-				view_error = "mini",
-				view_warn = "mini",
-				view_history = "messages",
-				view_search = false,
-			},
-			notify = {
-				enabled = true,
-				view = "mini",
-			},
-			lsp = {
-				progress = {
-					enabled = false,
-				},
-				hover = {
-					enabled = false,
-				},
-				signature = {
-					enabled = false,
-				},
-			},
-			popupmenu = {
-				enabled = false,
-			},
-			presets = {
-				bottom_search = false,
-				command_palette = false,
-				long_message_to_split = true,
-				inc_rename = false,
-				lsp_doc_border = false,
-			},
+			"rcarriga/nvim-notify",
 		},
 	},
 }
